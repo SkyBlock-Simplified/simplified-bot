be8c0ebca1b59ec277b948c15b8100a3959a5610
Nahydrin <6407029+Nahydrin@users.noreply.github.com>
2022-02-25 09:50:23 -0500

    Move slash command registration to the command registrar

---
+++ src/main/java/dev/sbs/simplifiedbot/command/group/developer/DevSlashCommand.java
@@ -0,0 +1,26 @@
+package dev.sbs.simplifiedbot.command.group.developer;
+
+import dev.sbs.discordapi.DiscordBot;
+import dev.sbs.discordapi.command.Command;
+import dev.sbs.discordapi.command.data.CommandInfo;
+import dev.sbs.discordapi.context.command.CommandContext;
+import dev.sbs.discordapi.util.exception.DiscordException;
+import dev.sbs.simplifiedbot.command.DevCommand;
+
+@CommandInfo(
+    id = "e7d01ff9-13f5-4b42-97c0-8c04faadf8ba",
+    name = "slash",
+    parent = DevCommand.class
+)
+public class DevSlashCommand extends Command {
+
+    protected DevSlashCommand(DiscordBot discordBot) {
+        super(discordBot);
+    }
+
+    @Override
+    protected void process(CommandContext<?> commandContext) throws DiscordException {
+        this.getDiscordBot().getCommandRegistrar().updateSlashCommands();
+    }
+
+}
