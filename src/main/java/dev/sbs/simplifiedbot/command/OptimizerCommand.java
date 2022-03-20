af8e0654cd50372e439201dbaa4e030182ffb673
Nahydrin <6407029+Nahydrin@users.noreply.github.com>
2022-03-20 04:44:17 -0400

    Added optimizer command class

--- src/main/java/dev/sbs/simplifiedbot/command/OptimizerCommand.java
+++ src/main/java/dev/sbs/simplifiedbot/command/OptimizerCommand.java
@@ -1,22 +1,22 @@
-package dev.sbs.simplifiedbot.command.group.player;
+package dev.sbs.simplifiedbot.command;
 
 import dev.sbs.discordapi.DiscordBot;
 import dev.sbs.discordapi.command.data.CommandInfo;
 import dev.sbs.discordapi.context.command.CommandContext;
 import dev.sbs.discordapi.response.Response;
-import dev.sbs.simplifiedbot.command.PlayerCommand;
+import dev.sbs.discordapi.response.page.Page;
 import dev.sbs.simplifiedbot.util.SkyBlockUser;
 import dev.sbs.simplifiedbot.util.SkyBlockUserCommand;
 import org.jetbrains.annotations.NotNull;
 import reactor.core.publisher.Mono;
 
 @CommandInfo(
-    id = "cc65f062-45f8-44c0-9635-84359e3ea246",
-    name = "dungeons"
+    id = "9171bc6d-fe0b-45e0-bf8f-bdbe87ccf064",
+    name = "optimizer"
 )
-public class PlayerDungeonsCommand extends SkyBlockUserCommand {
+public class OptimizerCommand extends SkyBlockUserCommand {
 
-    protected PlayerDungeonsCommand(DiscordBot discordBot) {
+    protected OptimizerCommand(DiscordBot discordBot) {
         super(discordBot);
     }
 
@@ -24,14 +24,11 @@ public class PlayerDungeonsCommand extends SkyBlockUserCommand {
     protected @NotNull Mono<Void> subprocess(@NotNull CommandContext<?> commandContext, @NotNull SkyBlockUser skyBlockUser) {
         return commandContext.reply(
             Response.builder()
-                .isInteractable()
-                .replyMention()
                 .withReference(commandContext)
                 .withPages(
-                    PlayerCommand.getPages(
-                        skyBlockUser,
-                        "dungeons"
-                    )
+                    Page.builder()
+                        .withContent("optimizer command")
+                        .build()
                 )
                 .build()
         );
