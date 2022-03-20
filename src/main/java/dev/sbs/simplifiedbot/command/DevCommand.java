5bb617fd2c3d1b269e67c057a73e25d64f12e5b7
Nahydrin <6407029+Nahydrin@users.noreply.github.com>
2022-03-20 04:55:53 -0400

    Use ParentCommand to ensure parent commands are never executed

--- src/main/java/dev/sbs/simplifiedbot/command/DevCommand.java
+++ src/main/java/dev/sbs/simplifiedbot/command/DevCommand.java
@@ -1,27 +1,20 @@
 package dev.sbs.simplifiedbot.command;
 
 import dev.sbs.discordapi.DiscordBot;
-import dev.sbs.discordapi.command.Command;
+import dev.sbs.discordapi.command.ParentCommand;
 import dev.sbs.discordapi.command.UserPermission;
 import dev.sbs.discordapi.command.data.CommandInfo;
-import dev.sbs.discordapi.context.command.CommandContext;
 import org.jetbrains.annotations.NotNull;
-import reactor.core.publisher.Mono;
 
 @CommandInfo(
     id = "a48552da-56bb-4262-b48f-05ad3dee5ff6",
     name = "dev",
     userPermissions = { UserPermission.DEVELOPER }
 )
-public class DevCommand extends Command {
+public class DevCommand extends ParentCommand {
 
-    protected DevCommand(DiscordBot discordBot) {
+    protected DevCommand(@NotNull DiscordBot discordBot) {
         super(discordBot);
-    }
-
-    @Override
-    protected @NotNull Mono<Void> process(@NotNull CommandContext<?> commandContext) {
-        return Mono.empty();
     }
 
 }
