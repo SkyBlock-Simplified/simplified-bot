a71110ae77832c7433dfbeabe59b686d2a118fe0
Nahydrin <6407029+Nahydrin@users.noreply.github.com>
2022-12-11 16:03:08 -0500

    Example code

--- src/main/java/dev/sbs/simplifiedbot/command/GuildCommand.java
+++ src/main/java/dev/sbs/simplifiedbot/command/GuildCommand.java
@@ -1,11 +1,23 @@
 package dev.sbs.simplifiedbot.command;
 
+import dev.sbs.api.SimplifiedApi;
+import dev.sbs.api.client.hypixel.implementation.HypixelPlayerData;
+import dev.sbs.api.client.hypixel.implementation.HypixelSkyBlockData;
+import dev.sbs.api.client.hypixel.response.hypixel.HypixelGuildResponse;
+import dev.sbs.api.client.hypixel.response.skyblock.island.SkyBlockIsland;
+import dev.sbs.api.client.sbs.implementation.MojangData;
+import dev.sbs.api.client.sbs.response.MojangProfileResponse;
+import dev.sbs.api.data.model.skyblock.skills.SkillModel;
+import dev.sbs.api.util.collection.concurrent.Concurrent;
+import dev.sbs.api.util.collection.concurrent.ConcurrentMap;
+import dev.sbs.api.util.data.tuple.Pair;
 import dev.sbs.discordapi.DiscordBot;
 import dev.sbs.discordapi.command.Command;
 import dev.sbs.discordapi.command.data.CommandInfo;
 import dev.sbs.discordapi.context.command.CommandContext;
 import dev.sbs.discordapi.response.Response;
 import dev.sbs.discordapi.response.page.Page;
+import dev.sbs.discordapi.response.page.PageItem;
 import org.jetbrains.annotations.NotNull;
 import reactor.core.publisher.Mono;
 
@@ -21,6 +33,45 @@ public class GuildCommand extends Command {
 
     @Override
     protected @NotNull Mono<Void> process(@NotNull CommandContext<?> commandContext) {
+        SkillModel combatSkillModel = SimplifiedApi.getRepositoryOf(SkillModel.class).findFirstOrNull(SkillModel::getKey, "COMBAT");
+        MojangData mojangData = SimplifiedApi.getWebApi(MojangData.class);
+        HypixelSkyBlockData skyBlockData = SimplifiedApi.getWebApi(HypixelSkyBlockData.class);
+        HypixelGuildResponse hypixelGuildResponse = SimplifiedApi.getWebApi(HypixelPlayerData.class).getGuildByName("SkyBlock Simplified");
+
+        if (hypixelGuildResponse.getGuild().isPresent()) {
+            HypixelGuildResponse.Guild guild = hypixelGuildResponse.getGuild().get();
+
+            ConcurrentMap<MojangProfileResponse, SkyBlockIsland> guildMembers = guild.getMembers()
+                .stream()
+                .map(member -> Pair.of(
+                    mojangData.getProfileFromUniqueId(member.getUniqueId()),
+                    skyBlockData.getProfiles(member.getUniqueId()).getLastPlayed())
+                )
+                .collect(Concurrent.toMap());
+
+            Response.builder()
+                .withReference(commandContext)
+                .withPages(
+                    Page.builder()
+                        .withItems(
+                            guildMembers.stream()
+                                .flatMap(member -> member.getValue()
+                                    .getMember(member.getKey().getUniqueId())
+                                    .stream()
+                                    .map(skyBlockMember -> skyBlockMember.getSkill(combatSkillModel).getLevel())
+                                    .map(combatLevel -> PageItem.builder()
+                                        .withLabel("Combat")
+                                        .withValue(String.valueOf(combatLevel))
+                                        .build()
+                                    )
+                                )
+                                .collect(Concurrent.toList())
+                        )
+                        .build()
+                )
+                .build();
+        }
+
         return commandContext.reply(
             Response.builder()
                 .withReference(commandContext)
