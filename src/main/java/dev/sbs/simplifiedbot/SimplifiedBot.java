b0a48135d82611c5347dd3e9e0eba2823e8d622c
Nahydrin <6407029+Nahydrin@users.noreply.github.com>
2022-12-15 15:31:05 -0500

    Add discord server to emoji table updating on load

--- src/main/java/dev/sbs/simplifiedbot/SimplifiedBot.java
+++ src/main/java/dev/sbs/simplifiedbot/SimplifiedBot.java
@@ -3,8 +3,15 @@ package dev.sbs.simplifiedbot;
 import dev.sbs.api.SimplifiedApi;
 import dev.sbs.api.client.sbs.implementation.SkyBlockData;
 import dev.sbs.api.client.sbs.response.SkyBlockEmojisResponse;
+import dev.sbs.api.data.model.discord.emojis.EmojiModel;
+import dev.sbs.api.data.model.discord.emojis.EmojiSqlModel;
+import dev.sbs.api.data.model.discord.guild_data.guilds.GuildModel;
+import dev.sbs.api.data.model.discord.guild_data.guilds.GuildSqlModel;
 import dev.sbs.api.util.collection.concurrent.Concurrent;
+import dev.sbs.api.util.collection.concurrent.ConcurrentList;
 import dev.sbs.api.util.collection.concurrent.ConcurrentSet;
+import dev.sbs.api.util.data.tuple.Pair;
+import dev.sbs.api.util.helper.WordUtil;
 import dev.sbs.discordapi.DiscordBot;
 import dev.sbs.discordapi.command.Command;
 import dev.sbs.discordapi.command.PrefixCommand;
@@ -31,6 +38,7 @@ import dev.sbs.simplifiedbot.command.group.player.PlayerSlayersCommand;
 import dev.sbs.simplifiedbot.command.group.player.PlayerWeightCommand;
 import dev.sbs.simplifiedbot.command.prefix.SbsCommand;
 import dev.sbs.simplifiedbot.util.ItemCache;
+import discord4j.common.util.Snowflake;
 import discord4j.core.object.presence.ClientActivity;
 import discord4j.core.object.presence.ClientPresence;
 import discord4j.gateway.ShardInfo;
@@ -116,7 +124,33 @@ public final class SimplifiedBot extends DiscordBot {
 
     @Override
     protected void onDatabaseConnected() {
+        // Update Emojis
+        this.getLog().info("Updating Emojis");
+        ConcurrentList<EmojiModel> allEmojis = SimplifiedApi.getRepositoryOf(EmojiModel.class).findAll();
+        SimplifiedApi.getRepositoryOf(GuildSqlModel.class)
+            .matchAll(GuildModel::isEmojiServer)
+            .stream()
+            .flatMap(guildModel -> this.getGateway()
+                .getGuildEmojis(Snowflake.of(guildModel.getGuildId()))
+                .filter(guildEmoji -> allEmojis.stream().noneMatch(emojiModel -> guildEmoji.getId().asLong() == emojiModel.getEmojiId()))
+                .map(guildEmoji -> Pair.of(guildModel, guildEmoji))
+                .collect(Concurrent.toList())
+                .blockOptional()
+                .orElse(Concurrent.newList())
+                .stream()
+            )
+            .forEach(guildEmoji -> {
+                EmojiSqlModel emojiSqlModel = new EmojiSqlModel();
+                emojiSqlModel.setGuild(guildEmoji.getKey());
+                emojiSqlModel.setEmojiId(guildEmoji.getRight().getId().asLong());
+                emojiSqlModel.setKey(guildEmoji.getRight().getName());
+                emojiSqlModel.setName(WordUtil.capitalizeFully(guildEmoji.getRight().getName().replace("_", " ")));
+                emojiSqlModel.setAnimated(guildEmoji.getRight().isAnimated());
+                emojiSqlModel.save();
+            });
+
         // Update Caches
+        this.getLog().info("Building Caches");
         this.skyBlockEmojis = SimplifiedApi.getWebApi(SkyBlockData.class).getEmojis();
         this.itemCache = new ItemCache();
         this.getItemCache().getAuctionHouse().update();
