50668d30a75a62419760355be8bd24671c9f6e04
Nahydrin <6407029+Nahydrin@users.noreply.github.com>
2023-06-05 01:15:28 -0400

    Add followup create, edit and delete testing

--- src/main/java/dev/sbs/simplifiedbot/command/developer/DevTestCommand.java
+++ src/main/java/dev/sbs/simplifiedbot/command/developer/DevTestCommand.java
@@ -52,16 +52,91 @@ public class DevTestCommand extends Command {
                             ActionRow.of(
                                 Button.builder()
                                     .withStyle(Button.Style.PRIMARY)
+                                    .withEmoji(Emoji.of(480079705077186560L, "chiefhmm"))
+                                    .withLabel("Create Followup")
+                                    .onInteract(context -> context.followup(
+                                        "followuptest",
+                                        Response.builder()
+                                            .withPages(
+                                                Page.builder()
+                                                    .withEmbeds(
+                                                        Embed.builder()
+                                                            .withDescription("Hmm")
+                                                            .withFooter("Herro")
+                                                            .build()
+                                                    )
+                                                    .withComponents(ActionRow.of(
+                                                        Button.builder()
+                                                            .withStyle(Button.Style.SECONDARY)
+                                                            .withEmoji(Emoji.of(769266796057985044L, "sip"))
+                                                            .withLabel("Edit Followup2")
+                                                            .onInteract(context2 -> context2.editFollowup(
+                                                                Response.builder()
+                                                                    .withPages(
+                                                                        Page.builder()
+                                                                            .withEmbeds(
+                                                                                Embed.builder()
+                                                                                    .withDescription("Hmm")
+                                                                                    .withFooter("Herro edit2")
+                                                                                    .build()
+                                                                            )
+                                                                            .build()
+                                                                    )
+                                                                    .build()
+                                                            ))
+                                                            .build(),
+                                                        Button.builder()
+                                                            .withStyle(Button.Style.DANGER)
+                                                            .withEmoji(Emoji.of(769266796057985044L, "sip"))
+                                                            .withLabel("Delete Followup2")
+                                                            .onInteract(ResponseContext::deleteFollowup)
+                                                            .build()
+                                                    ))
+                                                    .build()
+                                            )
+                                            .build()
+                                    ))
+                                    .build(),
+                                Button.builder()
+                                    .withStyle(Button.Style.SECONDARY)
+                                    .withEmoji(Emoji.of(769266796057985044L, "sip"))
+                                    .withLabel("Edit Followup")
+                                    .onInteract(context -> context.editFollowup(
+                                        "followuptest",
+                                        Response.builder()
+                                            .withPages(
+                                                Page.builder()
+                                                    .withEmbeds(
+                                                        Embed.builder()
+                                                            .withDescription("Hmm")
+                                                            .withFooter("Herro edit")
+                                                            .build()
+                                                    )
+                                                    .build()
+                                            )
+                                            .build()
+                                    ))
+                                    .build(),
+                                Button.builder()
+                                    .withStyle(Button.Style.DANGER)
+                                    .withEmoji(Emoji.of(769266796057985044L, "sip"))
+                                    .withLabel("Delete Followup")
+                                    .onInteract(context -> context.deleteFollowup("followuptest"))
+                                    .build()
+                            ),
+                            ActionRow.of(
+                                Button.builder()
+                                    .withStyle(Button.Style.PRIMARY)
                                     .withEmoji(Emoji.of("\uD83C\uDF85"))
                                     .withLabel("Santa")
                                     .withDeferEdit()
-                                    .onInteract(buttonContext -> this.editPage(buttonContext, pageBuilder -> pageBuilder.withContent("santa!")))
+                                    .onInteract(context -> this.editPage(context, pageBuilder -> pageBuilder.withContent("santa!")))
                                     .build(),
                                 Button.builder()
                                     .withStyle(Button.Style.SECONDARY)
                                     .withEmoji(Emoji.of("\uD83D\uDC31"))
                                     .withLabel("Cat")
-                                    .onInteract(buttonContext -> this.editPage(buttonContext, pageBuilder -> pageBuilder.withContent("cat!")))
+                                    .onInteract(context -> this.editPage(context, pageBuilder -> pageBuilder.withContent("cat!")))
                                     .build(),
                                 Button.builder()
                                     .withStyle(Button.Style.DANGER)
@@ -81,7 +156,7 @@ public class DevTestCommand extends Command {
                                                         .build()
                                                 )
                                             )
-                                            .onInteract(modalContext -> this.editPage(modalContext, pageBuilder -> pageBuilder.withContent("modal: " + modalContext.getComponent()
+                                            .onInteract(context -> this.editPage(context, pageBuilder -> pageBuilder.withContent("modal: " + context.getComponent()
                                                 .findComponent(TextInput.class, "something")
                                                 .flatMap(TextInput::getValue)
                                                 .orElse("hurdur i'm a failure")
@@ -109,20 +184,20 @@ public class DevTestCommand extends Command {
                                             .withLabel("Neigh")
                                             .withValue("value 1")
                                             .withEmoji(getEmoji("SKYBLOCK_ICON_HORSE"))
-                                            .onInteract(optionContext -> this.editPage(optionContext, pageBuilder -> pageBuilder.withContent(optionContext.getOption().getValue())))
+                                            .onInteract(context -> this.editPage(context, pageBuilder -> pageBuilder.withContent(context.getOption().getValue())))
                                             .build(),
                                         SelectMenu.Option.builder()
                                             .withLabel("Buni")
                                             .withValue("value 2")
                                             .withDescription("Looking for ores!")
                                             .withEmoji(Emoji.of(769279331875946506L, "Buni", true))
-                                            .onInteract(optionContext -> this.editPage(optionContext, pageBuilder -> pageBuilder.withContent(optionContext.getOption().getValue())))
+                                            .onInteract(context -> this.editPage(context, pageBuilder -> pageBuilder.withContent(context.getOption().getValue())))
                                             .build(),
                                         SelectMenu.Option.builder()
                                             .withLabel("Yes sir!")
                                             .withValue("value 3")
                                             .withEmoji(Emoji.of(837805777187241985L, "linasalute"))
-                                            .onInteract(optionContext -> this.editPage(optionContext, pageBuilder -> pageBuilder.withContent(optionContext.getOption().getValue())))
+                                            .onInteract(context -> this.editPage(context, pageBuilder -> pageBuilder.withContent(context.getOption().getValue())))
                                             .build(),
                                         SelectMenu.Option.builder()
                                             .withLabel("I do nothing :)")
