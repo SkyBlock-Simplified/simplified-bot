83416bdd407fdab7e105b019b35d5f0173997181
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2025-06-22 04:30:23 -0400

    Add helper methods to convert EmojiModel to discord Emoji

--- src/main/java/dev/sbs/simplifiedbot/data/discord/emojis/EmojiModel.java
+++ src/main/java/dev/sbs/simplifiedbot/data/discord/emojis/EmojiModel.java
@@ -1,8 +1,15 @@
 package dev.sbs.simplifiedbot.data.discord.emojis;
 
 import dev.sbs.api.data.model.Model;
+import dev.sbs.discordapi.context.reaction.ReactionContext;
 import dev.sbs.discordapi.response.Emoji;
 import dev.sbs.simplifiedbot.data.discord.guild_data.guilds.GuildModel;
+import discord4j.common.util.Snowflake;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import reactor.core.publisher.Mono;
+
+import java.util.function.Function;
 
 public interface EmojiModel extends Model {
 
@@ -16,8 +23,16 @@ public interface EmojiModel extends Model {
 
     boolean isAnimated();
 
-    default String getUrl() {
+    default @NotNull String getUrl() {
         return Emoji.getUrl(this.getEmojiId(), this.isAnimated());
+    }
+
+    default @NotNull Emoji getDiscordEmoji() {
+        return this.getDiscordEmoji(null);
+    }
+
+    default @NotNull Emoji getDiscordEmoji(@Nullable Function<ReactionContext, Mono<Void>> interaction) {
+        return Emoji.of(Snowflake.of(this.getEmojiId()), this.getKey(), this.isAnimated(), interaction);
     }
 
 }
