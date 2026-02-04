7d2603156a7cd9a2f0d021a328005d7838098c40
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2026-02-04 00:04:28 -0500

    Pet database models have been rebuilt as json

--- src/main/java/dev/sbs/simplifiedbot/data/skyblock/pet_data/pet_scores/PetScoreModel.java
+++
@@ -1,9 +0,0 @@
-package dev.sbs.simplifiedbot.data.skyblock.pet_data.pet_scores;
-
-import dev.sbs.api.data.model.Model;
-
-public interface PetScoreModel extends Model {
-
-    Integer getBreakpoint();
-
-}
