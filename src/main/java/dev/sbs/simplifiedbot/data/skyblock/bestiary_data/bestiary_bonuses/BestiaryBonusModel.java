eb1b73f557ecc979c68727ada2e3ceb4946ed7ff
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2026-03-08 21:01:06 -0400

    Delete more models

--- src/main/java/dev/sbs/simplifiedbot/data/skyblock/bestiary_data/bestiary_bonuses/BestiaryBonusModel.java
+++
@@ -1,9 +0,0 @@
-package dev.sbs.simplifiedbot.data.skyblock.bestiary_data.bestiary_bonuses;
-
-import dev.sbs.api.data.model.EffectsModel;
-
-public interface BestiaryBonusModel extends EffectsModel<Double> {
-
-    Integer getLevel();
-
-}
