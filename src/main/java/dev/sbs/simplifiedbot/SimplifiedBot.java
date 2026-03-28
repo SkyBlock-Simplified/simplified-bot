1363d2c698479907bc1304977906c0c77ffab771
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2026-03-28 17:23:24 -0400

    Cleanup bot creation

--- src/main/java/dev/sbs/simplifiedbot/SimplifiedBot.java
+++ src/main/java/dev/sbs/simplifiedbot/SimplifiedBot.java
@@ -1,26 +1,16 @@
 package dev.sbs.simplifiedbot;
 
 import dev.sbs.api.SimplifiedApi;
-import dev.sbs.api.client.impl.sbs.request.SbsRequest;
-import dev.sbs.api.client.impl.sbs.response.SkyBlockEmojis;
-import dev.sbs.api.collection.concurrent.Concurrent;
-import dev.sbs.api.collection.concurrent.ConcurrentList;
-import dev.sbs.api.data.model.discord.emojis.EmojiModel;
-import dev.sbs.api.data.model.discord.emojis.EmojiSqlModel;
-import dev.sbs.api.data.model.discord.guild_data.guilds.GuildModel;
-import dev.sbs.api.data.model.discord.guild_data.guilds.GuildSqlModel;
-import dev.sbs.api.data.sql.SqlConfig;
-import dev.sbs.api.mutable.pair.Pair;
+import dev.sbs.api.persistence.JpaConfig;
 import dev.sbs.api.reflection.Reflection;
 import dev.sbs.api.util.NumberUtil;
-import dev.sbs.api.util.StringUtil;
 import dev.sbs.api.util.SystemUtil;
 import dev.sbs.discordapi.DiscordBot;
-import dev.sbs.discordapi.DiscordConfig;
 import dev.sbs.discordapi.command.DiscordCommand;
-import dev.sbs.discordapi.response.Emoji;
+import dev.sbs.discordapi.handler.DiscordConfig;
+import dev.sbs.minecraftapi.client.sbs.SbsClient;
+import dev.sbs.minecraftapi.client.sbs.response.SkyBlockEmojiData;
 import dev.sbs.simplifiedbot.util.ItemCache;
-import discord4j.common.util.Snowflake;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.object.presence.ClientActivity;
 import discord4j.core.object.presence.ClientPresence;
@@ -39,69 +29,49 @@ import java.util.concurrent.TimeUnit;
 public final class SimplifiedBot extends DiscordBot {
 
     private ItemCache itemCache;
-    private SkyBlockEmojis skyBlockEmojis;
+    private SkyBlockEmojiData skyBlockEmojis;
+
+    private SimplifiedBot(@NotNull DiscordConfig discordConfig) {
+        super(discordConfig);
+    }
+
+    private static SimplifiedBot create(@NotNull DiscordConfig discordConfig) {
+        SimplifiedBot simplifiedBot = new SimplifiedBot(discordConfig);
+        simplifiedBot.start();
+        return simplifiedBot;
+    }
 
     public static void main(final String[] args) {
         DiscordConfig discordConfig = DiscordConfig.builder()
             .withToken(SystemUtil.getEnv("DISCORD_TOKEN"))
             .withMainGuildId(652148034448261150L)
-            .withDebugChannelId(SystemUtil.getEnv("DEVELOPER_ERROR_LOG_CHANNEL_ID").map(NumberUtil::tryParseLong))
-            .withDataConfig(SqlConfig.defaultSql())
+            .withLogChannelId(SystemUtil.getEnv("DEVELOPER_ERROR_LOG_CHANNEL_ID").map(NumberUtil::tryParseLong))
+            .withJpaConfig(JpaConfig.commonSql())
             .withCommands(
                 Reflection.getResources()
                     .filterPackage("dev.sbs.simplifiedbot.command")
                     .getTypesOf(DiscordCommand.class)
             )
+            .withEmojis(Reflection.getResources(SimplifiedBot.class.getClassLoader()).getResources("emojis/"))
             .withAllowedMentions(AllowedMentions.suppressEveryone())
             .withDisabledIntents(IntentSet.of(Intent.GUILD_PRESENCES))
             .withClientPresence(ClientPresence.online(ClientActivity.watching("commands")))
             .withMemberRequestFilter(MemberRequestFilter.withLargeGuilds())
             .build();
 
-        SimplifiedBot simplifiedBot = new SimplifiedBot();
-        simplifiedBot.setEmojiHandler(key -> SimplifiedApi.getRepositoryOf(EmojiModel.class)
-            .findFirst(EmojiModel::getKey, key)
-            .flatMap(Emoji::of)
-        );
-        simplifiedBot.login(discordConfig);
+        SimplifiedBot.create(discordConfig);
     }
 
     @Override
     protected void onGatewayConnected(@NotNull GatewayDiscordClient gatewayDiscordClient) {
-        SystemUtil.getEnv("HYPIXEL_API_KEY")
-            .map(StringUtil::toUUID)
-            .ifPresent(value -> SimplifiedApi.getKeyManager().add("HYPIXEL_API_KEY", value));
+        SimplifiedApi.getKeyManager().add(SystemUtil.getEnvPair("HYPIXEL_API_KEY"));
     }
 
     @Override
     protected void onDatabaseConnected() {
-        // Update Emojis
-        log.info("Updating Emojis");
-        ConcurrentList<EmojiModel> allEmojis = SimplifiedApi.getRepositoryOf(EmojiModel.class).findAll();
-        SimplifiedApi.getRepositoryOf(GuildSqlModel.class)
-            .matchAll(GuildModel::isEmojiServer)
-            .flatMap(guildModel -> this.getGateway()
-                .getGuildEmojis(Snowflake.of(guildModel.getGuildId()))
-                .filter(guildEmoji -> allEmojis.stream().noneMatch(emojiModel -> guildEmoji.getId().asLong() == emojiModel.getEmojiId()))
-                .map(guildEmoji -> Pair.of(guildModel, guildEmoji))
-                .collect(Concurrent.toList())
-                .blockOptional()
-                .orElse(Concurrent.newList())
-                .stream()
-            )
-            .forEach(guildEmoji -> {
-                EmojiSqlModel emojiSqlModel = new EmojiSqlModel();
-                emojiSqlModel.setGuild(guildEmoji.getKey());
-                emojiSqlModel.setEmojiId(guildEmoji.getRight().getId().asLong());
-                emojiSqlModel.setKey(guildEmoji.getRight().getName().replace(" ", "_").toUpperCase());
-                emojiSqlModel.setName(StringUtil.capitalizeFully(guildEmoji.getRight().getName().replace("_", " ")));
-                emojiSqlModel.setAnimated(guildEmoji.getRight().isAnimated());
-                emojiSqlModel.save();
-            });
-
         // Update Caches
         log.info("Building Caches");
-        this.skyBlockEmojis = SimplifiedApi.getApiRequest(SbsRequest.class).getItemEmojis();
+        this.skyBlockEmojis = SimplifiedApi.getClient(SbsClient.class).getEndpoint().getItemEmojis();
         this.itemCache = new ItemCache();
         this.getItemCache().getAuctionHouse().update();
         this.getItemCache().getBazaar().update();
@@ -109,7 +79,7 @@ public final class SimplifiedBot extends DiscordBot {
 
         // Schedule SkyBlock Emoji Cache Updates
         this.getScheduler().scheduleAsync(
-            () -> this.skyBlockEmojis = SimplifiedApi.getApiRequest(SbsRequest.class).getItemEmojis(),
+            () -> this.skyBlockEmojis = SimplifiedApi.getClient(SbsClient.class).getEndpoint().getItemEmojis(),
             10,
             10,
             TimeUnit.MINUTES
